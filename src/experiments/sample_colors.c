8066d4556bb465a0fc4d5198922ed65f6030c4b0
onixtsm <alexander@sautkin.com>
2024-05-02 12:48:50 +0200

    Sampled blocks

--- src/experiments/sample_colors.c
+++ src/experiments/sample_colors.c
@@ -4,49 +4,79 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <unistd.h>
-#include "util.h"
 
-#define IN IO_AR13
-#define ADC_IN ADC0
-#define S0 IO_AR7
-#define S1 IO_AR6
-#define S2 IO_AR4
-#define S3 IO_AR5
-#define CHECK IO_A1
+#define IN IO_AR9
+#define S0 IO_AR10
+#define S1 IO_AR11
+#define S2 IO_AR12
+#define S3 IO_AR13
 
-#define SAMPLE_SIZE 1000000
 
 typedef struct _data_t {
-  uint8_t level;
-  struct timeval time;
+  uint32_t dtw;
+  uint32_t dtr;
+  uint32_t dtb;
+  uint32_t dtg;
 } data_t;
 
-void print_data(const data_t* data, struct timeval start, const char* path) {
-  FILE* f = fopen(path, "wb");
-  long int dt;
-  for (size_t i = 0; i < SAMPLE_SIZE; ++i) {
-    dt = (data[i].time.tv_sec - start.tv_sec) * 1000 * 1000 + (data[i].time.tv_usec - start.tv_usec);
-    fprintf(f, "%ld,%d\n", dt, data[i].level);
-  }
-}
-
-void get_data(uint8_t s, char *name) {
-  data_t* data = malloc(SAMPLE_SIZE * sizeof(*data));
-
-  gpio_set_level(S2, (s >> 1) & 1);
-  gpio_set_level(S3, s & 1);
-  struct timeval check, start;
+uint32_t get_preiod(uint8_t pin, uint8_t level) {
+  struct timeval end, start;
   gettimeofday(&start, NULL);
-  for (size_t i = 0; i < SAMPLE_SIZE; ++i) {
-    data[i].level = gpio_get_level(IN);
-    gettimeofday(&check, NULL);
-    data[i].time = check;
-  }
-  print_data(data, start, name);
-  free(data);
+
+  uint8_t curr_level;
+  do {
+    curr_level = gpio_get_level(pin);
+  } while (curr_level != level);
+  gettimeofday(&start, NULL);
+  do {
+  curr_level = gpio_get_level(pin);
+  } while (curr_level == level);
+
+  gettimeofday(&end, NULL);
+
+  return (end.tv_sec - start.tv_sec) * 1000 * 1000 + (end.tv_usec - start.tv_usec);
 }
 
-int main(void) {
+void print_data(const data_t *data, const char *path, size_t sample_size) {
+  FILE *f = fopen(path, "wb");
+  for (size_t i = 0; i < sample_size; ++i) {
+    fprintf(f, "%d,%d,%d,%d\n", data[i].dtw, data[i].dtr, data[i].dtb, data[i].dtg);
+  }
+  fclose(f);
+}
+
+void sample_data(const char *path, size_t sample_size) {
+  data_t *buffer = malloc(sizeof(*buffer) * sample_size);
+  for (size_t i = 0; i < sample_size; ++i) {
+    gpio_set_level(S2, 1);
+    gpio_set_level(S3, 0);
+    buffer[i].dtw = get_preiod(IN, 1);
+
+    gpio_set_level(S2, 0);
+    gpio_set_level(S3, 0);
+    buffer[i].dtr = get_preiod(IN, 1);
+
+    gpio_set_level(S2, 0);
+    gpio_set_level(S3, 1);
+    buffer[i].dtb = get_preiod(IN, 1);
+
+    gpio_set_level(S2, 1);
+    gpio_set_level(S3, 1);
+    buffer[i].dtg = get_preiod(IN, 1);
+  }
+  print_data(buffer, path, sample_size);
+  free(buffer);
+}
+
+int main(int argc, char** argv) {
+  size_t sample_size;
+
+  if (argc == 1) {
+    sample_size = 1000;
+  } else {
+    sample_size = atol(argv[1]);
+  }
+
   pynq_init();
   gpio_init();
   adc_init();
@@ -57,16 +87,11 @@ int main(void) {
   gpio_set_direction(S1, GPIO_DIR_OUTPUT);
   gpio_set_direction(S2, GPIO_DIR_OUTPUT);
   gpio_set_direction(S3, GPIO_DIR_OUTPUT);
-  gpio_set_direction(CHECK, GPIO_DIR_OUTPUT);
 
   gpio_set_level(S0, GPIO_LEVEL_LOW);
   gpio_set_level(S1, GPIO_LEVEL_HIGH);
 
-  // No filter
-  get_data(0, "red.csv");
-  get_data(0b01, "blue.csv");
-  get_data(0b10, "white.csv");
-  get_data(0b11, "green.csv");
+  sample_data("data.csv", sample_size);
 
   pynq_destroy();
 }
