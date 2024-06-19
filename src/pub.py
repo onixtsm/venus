62e4dbba9388936108619a80e99c9b74b2865b02
Alexandru Dicu <m.alexandrudicu@gmail.com>
2024-06-19 21:16:47 +0200

    Updated the server-side communication to sync with the comms lib

--- src/pub.py
+++ src/pub.py
@@ -1,20 +1,23 @@
 import paho.mqtt.client as mqtt
 import json
+import time
 
 
 ##Variable to connect to MQTT host
 broker_address = "mqtt.ics.ele.tue.nl"
 broker_port = 1883
 topic = "/pynqbridge/67/recv"
-username = "student129"
+username = "Student129"
 password = "xaingu0I"
 
 ##Arbitraty data for data dictionaryy
-coord_x = 1
-coord_y = 5
-object_found = 1
-object_color = 3
-status = 5
+coord_x = 2
+coord_y = 30
+object_found = 3
+object_color = 4
+status = 0
+obstacle_x= 1
+obstacle_y= 3
 
 ##Check if connection worked
 def on_connect(client, userdata, flags,rc):
@@ -33,16 +36,20 @@ def on_publish(client, userdata, mid):
 def send_msg(client, topic, data):
     json_string  = json.dumps(data)
     byte_data = json_string.encode('utf-8')
+   
     client.publish(topic, byte_data)
        
 
 #data
 data = {  
-    "x": coord_x,
-    "y": coord_y,
-    "obj_found": object_found,
-    "color": object_color,
-    "status": status,
+    "robot_x": coord_x,
+    "robot_y": coord_y,
+    "robot_status": status,
+    "obstacle_x": obstacle_x,
+    "obstacle_y": obstacle_y,
+    "obstacle_type": object_found,
+    "obstacle_color": object_color,
+    
  }
 
 
@@ -53,7 +60,13 @@ client.connect(broker_address, broker_port) #Connect to server
 client.on_connect = on_connect #Check if connected succesfuly
 client.on_publish = on_publish #Setup publish command to give feedback on message publication
 
-send_msg(client, topic, data)
+x = 0
+while x != 10:  
+    send_msg(client, topic, data)
+    data['robot_status'] += 1
+    x += 1
+    time.sleep(0.5)
+
 
 
 
